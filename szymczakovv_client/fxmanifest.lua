0a742c3def17195a22197d84f511778928d92c0f
tekken420weed <74188150+tekken420weed@users.noreply.github.com>
2021-08-28 09:41:50 +0200

    Update fxmanifest.lua
    useless linijki

--- szymczakovv_client/fxmanifest.lua
+++ szymczakovv_client/fxmanifest.lua
@@ -4,10 +4,5 @@ lua54 'yes'
 author 'szymczakovv#1937'
 description 'Load Client Script from Server-Side'
 
-client_scripts {
-    'client.lua'
-}
-
-server_scripts {
-    'server.lua'
-}
+client_script 'client.lua'
+server_script 'server.lua'
